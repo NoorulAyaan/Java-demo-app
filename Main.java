6ae2ab862a4cc85eab1637ea9e929ef0c19c7496
Noor ul Ayaan <Ayan.cloudlem@gmail.com>
2025-02-06 20:26:09 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        // Simple addition
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
